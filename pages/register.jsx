a54b2cabe1b1b6eca3d1cf5c0fa1027caf72d719
Juraj Jukic <juraj.jukic1@gmail.com>
2022-10-13 14:46:53 +0200

    feat(registration): connect registration screen with backend

--- pages/register.jsx
+++ pages/register.jsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import Image from 'next/image'
 import Link from 'next/link';
 import Button from '@mui/material/Button';
@@ -11,23 +12,39 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import style from '../styles/Sign.module.scss';
 import Copyright from '../components/UI/copyright';
+import { register } from '../store/actions/authActions';
 
 import Logo from '../assets/images/logo.png';
 
 const theme = createTheme();
 
-const SignUp = () => {
-  const handleSubmit = (event) => {
-    console.log('event', event);
-  };
+const SignUpContainer = (props) => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: ""
+  })
+
+  const handleChange = (evt) => {
+    const value = evt.target.value;
+    setState({
+      ...state,
+      [evt.target.name]: value
+    })
+  } 
+
+  const onRegister = (registerForm) => {
+    props.register(registerForm)
+  }
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+      <Container component="register" maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
-            marginTop: 8,
+            // marginTop: 8,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
@@ -37,7 +54,7 @@ const SignUp = () => {
           <Typography component="h1" variant="h5" sx={{ mt: 5 }}>
             Sign up
           </Typography>
-          <Box noValidate onClick={() => handleSubmit()} sx={{ mt: 3 }}>
+          <Box noValidate sx={{ mt: 3 }}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -48,6 +65,8 @@ const SignUp = () => {
                   id="firstName"
                   label="First Name"
                   autoFocus
+                  value={state.firstName}
+                  onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -58,6 +77,8 @@ const SignUp = () => {
                   label="Last Name"
                   name="lastName"
                   autoComplete="family-name"
+                  value={state.lastName}
+                  onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -68,6 +89,8 @@ const SignUp = () => {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
+                  value={state.email}
+                  onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -79,6 +102,8 @@ const SignUp = () => {
                   type="password"
                   id="password"
                   autoComplete="new-password"
+                  value={state.password}
+                  onChange={handleChange}
                 />
               </Grid>
             </Grid>
@@ -87,6 +112,7 @@ const SignUp = () => {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              onClick={() => onRegister(state)} 
             >
               Sign Up
             </Button>
@@ -105,4 +131,12 @@ const SignUp = () => {
   );
 }
 
-export default SignUp;
+const mapStateToProps = ({ authState }) => ({
+  authState
+})
+
+const mapDispatchToProps = {
+  register,
+}
+
+export default connect(undefined, mapDispatchToProps)(SignUpContainer)
